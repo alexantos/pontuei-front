f23697cf184f12af7d579bae2e9c10a9dd4c3e66
alexantos <xandreale09@gmail.com>
2025-04-26 02:32:05 -0300

    Configuração inicial.

---
+++ src/app/selecao/selecao.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+
+
+@Component({
+  selector: 'app-selecao',
+  imports: [
+    MatCardModule
+  ],
+  templateUrl: './selecao.component.html',
+  styleUrl: './selecao.component.css'
+})
+export class SelecaoComponent {
+
+}
